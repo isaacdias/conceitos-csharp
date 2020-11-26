be30967b6f1a11bc3b5e6e0a190ddd671f6aeb00
Isaac Dias <isaacardias@gmail.com>
2020-11-25 21:25:43 -0300

    add funcionalidade de gerar media geral

--- Revisao/Program.cs
+++ Revisao/Program.cs
@@ -44,7 +44,41 @@ namespace Revisao
             }
             break;
           case "3":
-            // gerar media geral
+            decimal notaTotal = 0;
+            var qtdAlunos = 0;
+            for (int i = 0; i < alunos.Length; i++)
+            {
+              if (!string.IsNullOrEmpty(alunos[i].Nome))
+              {
+                notaTotal = notaTotal + alunos[i].Nota;
+                qtdAlunos++;
+              }
+            }
+            var mediaGeral = notaTotal / qtdAlunos;
+            Conceito conceitoGeral;
+
+            if (mediaGeral < 2)
+            {
+              conceitoGeral = Conceito.E;
+            }
+            else if (mediaGeral < 4)
+            {
+              conceitoGeral = Conceito.D;
+            }
+            else if (mediaGeral < 6)
+            {
+              conceitoGeral = Conceito.C;
+            }
+            else if (mediaGeral < 8)
+            {
+              conceitoGeral = Conceito.B;
+            }
+            else
+            {
+              conceitoGeral = Conceito.A;
+            }
+
+            Console.WriteLine($"MÉDIA GERAL: {mediaGeral} - CONCEITO: {conceitoGeral}");
             break;
           default:
             throw new ArgumentOutOfRangeException();
