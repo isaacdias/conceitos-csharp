b19f2931ad32824cd9c59ecc41967c40e6d70f80
Isaac Dias <isaacardias@gmail.com>
2020-11-25 21:02:38 -0300

    add funcionalidade para listar alunos

--- Revisao/Program.cs
+++ Revisao/Program.cs
@@ -35,7 +35,13 @@ namespace Revisao
 
             break;
           case "2":
-            // listar alunos
+            foreach (var a in alunos)
+            {
+              if (!string.IsNullOrEmpty(a.Nome))
+              {
+                Console.WriteLine($"ALUNO: {a.Nome} - NOTA: {a.Nota}");
+              }
+            }
             break;
           case "3":
             // gerar media geral
